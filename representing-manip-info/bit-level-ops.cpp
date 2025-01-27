d655fa6aa50ddb43736c54253aa86509c444e08c
ChristianWolff1 <131631389+ChristianWolff1@users.noreply.github.com>
2025-01-27 09:20:17 -0700

    Update bit-level-ops.cpp
    reverse array C template has been added. Needed to be tested.

--- representing-manip-info/bit-level-ops.cpp
+++ representing-manip-info/bit-level-ops.cpp
@@ -8,3 +8,8 @@ void inplace_swap(int *x, int *y) {
   *x = *y ^ *x;
   *y = *x ^ *y;
 }
+
+/** reverse_array: the primal function to reverse any set of bytes. It could be molded to any type. */
+void reverse_array(int *a, int size) {
+  for (int first{}, last{ size - 1}; first < last; ++first, --last) { inplace_bswap(&a[first], &a[last]); }
+}
